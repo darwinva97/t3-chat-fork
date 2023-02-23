545c8c7b2d823a65c963def192f95059ef0eed77
darwinva97 <darwin97.va@hotmail.com>
2023-02-23 17:09:10 -0500

    add production in NODE_ENV environment

--- src/server/env-schema.js
+++ src/server/env-schema.js
@@ -2,7 +2,7 @@ const { z } = require("zod");
 
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
-  // NODE_ENV: z.enum(["development", "test", "production"]),
+  NODE_ENV: z.enum(["development", "test", "production"]),
   NEXTAUTH_SECRET: z.string(),
   NEXT_PUBLIC_WS_URL: z.string(),
   NEXTAUTH_URL: z.string().url(),
